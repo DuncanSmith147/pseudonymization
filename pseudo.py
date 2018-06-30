850fdf78584458db9d6487b38180e781c76eb610
DuncanSmith147 <duncan.g.smith@manchester.ac.uk>
2018-06-30 18:47:23 +0100

    Added caching for Minwise hashing and subclasses
    Added caching. Also added the possibility of alternatives to simple
    tabulation hashing, and added a function to allow Jaccard score
    estimation for b-bit hashes.

--- pseudo.py
+++ pseudo.py
@@ -29,7 +29,7 @@ from set_like import BloomFilter
 
 
 class Minwise(object):
-    def __init__(self, m, q=64, seed=None):
+    def __init__(self, m, q=64, seed=None, klass=SimpleTabulation):
         # m is the length of the list of hashes returned by the hash method
         # if seed is not None, then its value will
         # be used to seed the PRNG
@@ -38,13 +38,21 @@ class Minwise(object):
             raise ValueError('Minwise hash must have length > 0')
         if not seed is None:
             np.random.seed(seed)
-        self.hashers = [SimpleTabulation(q=q) for _ in range(m)]
+        self._hashers = [klass(q=q) for _ in range(m)]
+        self._cache = {}
+
+    @property
+    def hashers(self):
+        return self._hashers
 
     def hash(self, tokens):
         # returns a list of minwise hashes
-        # ensure tokens is a set
-        tokens = set(tokens)
-        return [min(h.hash(s) for s in tokens) for h in self.hashers]
+        # ensure tokens is a frozenset
+        if not isinstance(tokens, frozenset):
+            tokens = frozenset(tokens)
+        if not tokens in self._cache:
+            self._cache[tokens] = [min(h.hash(s) for s in tokens) for h in self.hashers]
+        return self._cache[tokens]
 
 
 class B_bit(Minwise):
@@ -54,8 +62,12 @@ class B_bit(Minwise):
 
     def hash(self, tokens):
         # returns a list of b-bit minwise hashes
-        mod = self._mod
-        return [x % mod for x in super(B_bit, self).hash(tokens)]
+        if not isinstance(tokens, frozenset):
+            tokens = frozenset(tokens)
+        if not tokens in self._cache:
+            mod = self._mod
+            self._cache[tokens] = [x % mod for x in super(B_bit, self).hash(tokens)]
+        return self._cache[tokens]
 
 
 class Concatenated(B_bit):
@@ -64,11 +76,15 @@ class Concatenated(B_bit):
 
     def hash(self, tokens):
         # returns a concatenated 1-bit hash
-        h = 0
-        for i, bit in enumerate(reversed(super(Concatenated, self).hash(tokens))):
-            if bit:
-                h += (1 << i)
-        return C_hash(h, len(self.hashers))
+        if not isinstance(tokens, frozenset):
+            tokens = frozenset(tokens)
+        if not tokens in self._cache:
+            h = 0
+            for i, bit in enumerate(reversed(super(Concatenated, self).hash(tokens))):
+                if bit:
+                    h += (1 << i)
+            self._cache[tokens] = C_hash(h, len(self.hashers))
+        return self._cache[tokens]
 
 
 class C_hash(long):
@@ -124,6 +140,9 @@ class C_hash(long):
         return self._N
 
 
+############## Functions based on token sets ##############
+
+
 def Jaccard(tokens1, tokens2):
     # returns exact Jaccard
     # similarity measure for
@@ -132,12 +151,47 @@ def Jaccard(tokens1, tokens2):
     tokens2 = set(tokens2)
     return len(tokens1&tokens2) / len(tokens1|tokens2)
 
+def dice_coefficient(tokens1, tokens2):
+    # returns exact dice coefficient
+    # similarity measure for
+    # two token sets
+    tokens1 = set(tokens1)
+    tokens2 = set(tokens2)
+    return 2 * len(tokens1&tokens2) / (len(tokens1) + len(tokens2))
+
+
+############## Functions for b-bit hashes ##############
+
+
+def J_hat(hashes1, hashes2, b):
+    # returns estimated Jaccard score from lists of
+    # minwise hashes 
+    # hashes1 and hashes2 are lists for different
+    # token sets using the same (minwise) hash functions
+    # only the b most significant bits are used
+    # in the evaluation of the measure
+    # ensuring that b is 'much' less than the
+    # number of bits in the full hash values
+    # assumes the probability of collisions on the
+    # full hash is negligible
+    if not len(hashes1) == len(hashes2):
+        raise ValueError('Hash lists must have equal length')
+    mod = 2**b
+    frac = sum([(a-b) % mod == 0 for (a,b) in zip(hashes1, hashes2)]) / len(hashes1)
+    c = (1/2)**b
+    return (frac-c)/(1-c)
+
+
+############## Functions for 1-bit concatenated hashes ##############
+
+
 def J_hat_from_conc(a, b, m, N=1, truncate=True):
     # returns estimated Jaccard
     # similarity measure for
     # a pair of comparable concatenated hashes
     # if truncate is True, then 0 is
     # returned rather than a negative value
+    # N is the compression factor
     try:
         res = (1-2*hamdist(a,b)/m)**(1/N)
     except ValueError:
